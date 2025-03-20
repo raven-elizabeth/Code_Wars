d63915a3c6196be778d075091159540a9f0d0bbb
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2025-03-20 18:02:53 +0000

    Added comments

--- Solutions/regex_validate_pin.py
+++ Solutions/regex_validate_pin.py
@@ -1,11 +1,13 @@
 # ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 # If the function is passed a valid PIN string, return true, else return false.
 
+
 # My first solution:
 def validate_with_isdigit(pin):
     if (len(pin) == 4 or len(pin) == 6) and pin.isdigit():
         return True
     return False
+
 
 # RegEx solution:
 import re
@@ -18,7 +20,13 @@ def validate_with_regex(pin):
         return True
     return False
 
-print(validate_with_isdigit("1234"))
-print(validate_with_isdigit("12345"))
-print(validate_with_regex("1234"))
-print(validate_with_regex("12345"))
+
+# Alternatives:
+# def validate_pin(pin):
+#     return len(pin) in (4, 6) and pin.isdigit()
+
+
+# if re.fullmatch("\d{4}|\d{6}", pin):
+#         return True
+#     else:
+#         return False
