9441cad4e833f642e6e8f6196f17a3833e6d42b5
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2025-09-16 21:16:27 +0100

    Added first Java challenge

---
+++ Solutions/Java/HelloWorld.java
@@ -0,0 +1,9 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+package Solutions.Java;
+
+public class HelloWorld {
+    public static String greet(){
+    return "hello world!";
+    }
+}
